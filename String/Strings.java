dba29975ca0bed79ab97a56a645d3aa366312a96
bilawal21 <bilawal.ec21@gmail.com>
2025-03-10 03:19:50 +0500

    Implement StringBuilder operations and demonstrate character manipulation

--- String/Strings.java
+++ String/Strings.java
@@ -1,5 +1,34 @@
 public class Strings {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+
+        System.out.println(sb);
+        System.out.println(sb.charAt(0));
+
+        sb.setCharAt(0, 'P');
+        System.out.println(sb);
+
+        sb.insert(0, 'T');
+        System.out.println(sb);
+
+        sb.delete(1, 2);
+        System.out.println(sb);
+
+        sb.append('y');
+        System.out.println(sb.length());
+
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+
+        }
+        System.out.println(sb);
         
     }
 }
